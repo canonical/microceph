6048807db6283b5f396d7d0c80f92ebd341ac06c
Stéphane Graber <stephane.graber@canonical.com>
2022-09-29 22:20:09 -0400

    status: Add status command
    Closes #25
    
    Signed-off-by: Stéphane Graber <stephane.graber@canonical.com>

--- microceph/cmd/microceph/status.go
+++ microceph/cmd/microceph/status.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/canonical/microcluster/microcluster"
 	"github.com/spf13/cobra"
+
+	"github.com/canonical/microceph/microceph/client"
 )
 
 type cmdStatus struct {
@@ -19,5 +27,66 @@ func (c *cmdStatus) Command() *cobra.Command {
 }
 
 func (c *cmdStatus) Run(cmd *cobra.Command, args []string) error {
+	m, err := microcluster.App(context.Background(), c.common.FlagStateDir, c.common.FlagLogVerbose, c.common.FlagLogDebug)
+	if err != nil {
+		return err
+	}
+
+	cli, err := m.LocalClient()
+	if err != nil {
+		return err
+	}
+
+	// Get configured disks.
+	disks, err := client.GetDisks(context.Background(), cli)
+	if err != nil {
+		return err
+	}
+
+	// Get services.
+	services, err := client.GetServices(context.Background(), cli)
+	if err != nil {
+		return err
+	}
+
+	// Get cluster members.
+	clusterMembers, err := cli.GetClusterMembers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("MicroCeph deployment summary:")
+
+	for _, server := range clusterMembers {
+		// Disks.
+		diskCount := 0
+		for _, disk := range disks {
+			if disk.Location != server.Name {
+				continue
+			}
+
+			diskCount++
+		}
+
+		// Services.
+		srvServices := []string{}
+		for _, service := range services {
+			if service.Location != server.Name {
+				continue
+			}
+
+			srvServices = append(srvServices, service.Service)
+		}
+		sort.Strings(srvServices)
+
+		if diskCount > 0 {
+			srvServices = append(srvServices, "osd")
+		}
+
+		fmt.Printf("- %s (%s)\n", server.Name, server.Address.Addr().String())
+		fmt.Printf("  Services: %s\n", strings.Join(srvServices, ", "))
+		fmt.Printf("  Disks: %d\n", diskCount)
+	}
+
 	return nil
 }
