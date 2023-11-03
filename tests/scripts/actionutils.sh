67b9a8abba31109f16355d0956280db2f187ba71
Utkarsh Bhatt <utkarsh.bhatt@canonical.com>
2023-11-03 17:35:22 +0530

    Added CI test for --mon-ip parameter using separate lxd network
    Signed-off-by: Utkarsh Bhatt <utkarsh.bhatt@canonical.com>

--- tests/scripts/actionutils.sh
+++ tests/scripts/actionutils.sh
@@ -74,8 +74,14 @@ function enable_rgw() {
 }
 
 function create_containers() {
+    # Create public network for internal ceph cluster
+    lxc network create public
+    nw=$(lxc network list --format=csv | grep "public" | cut -d, -f4)
+    gw=$(echo $nw | cut -d/ -f1)
+    mask=$(echo $nw | cut -d/ -f2)
     # Create head node and 3 worker containers
-    for container in node-head node-wrk1 node-wrk2 node-wrk3 ; do
+    for i in 0 1 2 3 ; do
+        container="node-wrk${i}" # node name
         # Bring up privileged containers
         lxc init ubuntu:22.04 $container
         lxc config set $container security.privileged true
@@ -83,9 +89,13 @@ function create_containers() {
         # Allow access to loopback devices
         printf 'lxc.cgroup2.devices.allow = b 7:* rwm\nlxc.cgroup2.devices.allow = c 10:237 rwm' | lxc config set $container raw.lxc - 
 
-        # Mount home and start
+        # Configure and start container
         lxc config device add $container homedir disk source=${HOME} path=/mnt
+        lxc network attach public $container eth2
         lxc start $container
+
+        # allocate ip address on the attached network
+        lxc exec $container -- sh -c "ip a add ${gw}${i}/${mask} dev"
 
         # Create loopback devices on the host but access through container
         loop_file="$(sudo mktemp -p /mnt mctest-${i}-XXXX.img)"
@@ -103,7 +113,7 @@ function create_containers() {
 
 function install_multinode() {
     # Install and setup microceph snap
-    for container in node-head node-wrk1 node-wrk2 node-wrk3 ; do
+    for container in node-wrk0 node-wrk1 node-wrk2 node-wrk3 ; do
         lxc exec $container -- sh -c "sudo snap install --dangerous /mnt/microceph_*.snap"
         lxc exec $container -- sh -c "snap connect microceph:block-devices ; snap connect microceph:hardware-observe"
         # Hack: allow access to sysfs hardware info through lxc
@@ -115,21 +125,21 @@ function install_multinode() {
 
 function install_store() {
     local chan="${1?missing}"
-    for container in node-head node-wrk1 node-wrk2 node-wrk3 ; do
+    for container in node-wrk0 node-wrk1 node-wrk2 node-wrk3 ; do
         lxc exec $container -- sh -c "sudo snap install microceph --channel ${chan}"
     done
 }
 
 function refresh_snap() {
     local chan="${1?missing}"
-    for container in node-head node-wrk1 node-wrk2 node-wrk3 ; do
+    for container in node-wrk0 node-wrk1 node-wrk2 node-wrk3 ; do
         lxc exec $container -- sh -c "sudo snap refresh microceph --channel ${chan}"
     done
 }
 
 function check_client_configs() {
     # Issue cluster wide client config set.
-    lxc exec node-head -- sh -c "microceph client config set rbd_cache true"
+    lxc exec node-wrk0 -- sh -c "microceph client config set rbd_cache true"
     # Issue host specific client config set for each worker node.
     for id in 1 2 3 ; do
         lxc exec node-wrk${id} -- sh -c "microceph client config set rbd_cache_size $((512*$id))"
@@ -146,8 +156,8 @@ function check_client_configs() {
     done
 
     # Reset client configs
-    lxc exec node-head -- sh -c "microceph client config reset rbd_cache"
-    lxc exec node-head -- sh -c "microceph client config reset rbd_cache_size"
+    lxc exec node-wrk0 -- sh -c "microceph client config reset rbd_cache"
+    lxc exec node-wrk0 -- sh -c "microceph client config reset rbd_cache_size"
 
     # Verify client configs post reset on each node.
     for id in 1 2 3 ; do
@@ -161,21 +171,57 @@ function check_client_configs() {
 }
 
 function bootstrap_head() {
+    set -x
+
     # Bootstrap microceph on the head node
-    lxc exec node-head -- sh -c "microceph cluster bootstrap"
-    lxc exec node-head -- sh -c "microceph status"
+    nw=$(lxc network list --format=csv | grep "public" | cut -d, -f4)
+    gw=$(echo $nw | cut -d/ -f1)
+    mask=$(echo $nw | cut -d/ -f2)
+    mon_ip="${gw}0"
+    lxc exec node-wrk0 -- sh -c "microceph cluster bootstrap --mon-ip $mon_ip"
+    lxc exec node-wrk0 -- sh -c "microceph status"
     sleep 4
-    lxc exec node-head -- sh -c 'microceph.ceph -s | egrep "mon: 1 daemons, quorum node-head"'
+    lxc exec node-wrk0 -- sh -c 'microceph.ceph -s | egrep "mon: 1 daemons, quorum node-wrk0"'
+
+    # Verify ceph.conf
+    res1=$(lxc exec node-wrk0 -- sh -c "cat /var/snap/microceph/current/conf/ceph.conf | grep -c 'mon host = $mon_ip'")
+    res2=$(lxc exec node-wrk0 -- sh -c "cat /var/snap/microceph/current/conf/ceph.conf | grep -c 'public_network = ${mon_ip}/${mask}'")
+    if (($res1 -ne "1")) || (($res2 -ne "1")) ; then
+        # required configs not present.
+        exit 1
+    fi
 }
 
 function cluster_nodes() {
+    set -x
+
     # Add/join microceph nodes to the cluster
+    nw=$(lxc network list --format=csv | grep "public" | cut -d, -f4)
+    gw=$(echo $nw | cut -d/ -f1)
+    mask=$(echo $nw | cut -d/ -f2)
+    bootstrap_ip="${gw}0"
+    mon_ips=${bootstrap_ip}
     for i in 1 2 3 ; do
-        tok=$( lxc exec node-head -- sh -c "microceph cluster add node-wrk${i}" )
+        node_mon_ip="${gw}${i}/${mask}"
+
+        # join MicroCeph cluster
+        tok=$(lxc exec node-wrk0 -- sh -c "microceph cluster add node-wrk${i}" )
         lxc exec node-wrk${i} -- sh -c "microceph cluster join $tok"
+
+        # verify ceph.conf
+        res1=$(lxc exec node-wrk${id} -- sh -c "cat /var/snap/microceph/current/conf/ceph.conf | grep -c 'mon host = $mon_ips'")
+        res2=$(lxc exec node-wrk${id} -- sh -c "cat /var/snap/microceph/current/conf/ceph.conf | grep -c 'public_network = ${bootstrap_ip}/${mask}'")
+        if (($res1 -ne "0")) || (($res2 -ne "0")) ; then
+            # Incorrect configs present.
+            exit 1
+        fi
+
+        # append mon_ips
+        mon_ips="$mon_ips,$node_mon_ip"
     done
+
     for i in $(seq 1 8); do
-        res=$( ( lxc exec node-head -- sh -c 'microceph status | grep -cE "^- node"' ) || true )
+        res=$( ( lxc exec node-wrk0 -- sh -c 'microceph status | grep -cE "^- node"' ) || true )
         if [[ $res -gt 3 ]] ; then
             echo "Found >3 nodes"
             break
@@ -184,8 +230,9 @@ function cluster_nodes() {
             sleep 2
         fi
     done
-    lxc exec node-head -- sh -c 'microceph status'
-    lxc exec node-head -- sh -c 'microceph.ceph -s'
+
+    lxc exec node-wrk0 -- sh -c 'microceph status'
+    lxc exec node-wrk0 -- sh -c 'microceph.ceph -s'
 }
 
 function add_osd_to_node() {
@@ -302,7 +349,7 @@ function headexec() {
     local run="${1?missing}"
     shift
     set -x
-    lxc exec node-head -- sh -c "/mnt/actionutils.sh $run $@"
+    lxc exec node-wrk0 -- sh -c "/mnt/actionutils.sh $run $@"
 }
 
 run="${1}"
