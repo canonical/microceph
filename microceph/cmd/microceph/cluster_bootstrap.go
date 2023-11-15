2ca36026a4e859980c6bce4add62644f345a543c
Peter Sabaini <peter.sabaini@canonical.com>
2023-11-15 17:20:01 +0100

    Try harder when clustering
    Increase timeouts on clustering operations. Setting up the dqlite
    infra can be resource intensive and might land into timeouts on loaded
    systems otherwise.
    
    Signed-off-by: Peter Sabaini <peter.sabaini@canonical.com>

--- microceph/cmd/microceph/cluster_bootstrap.go
+++ microceph/cmd/microceph/cluster_bootstrap.go
@@ -46,5 +46,5 @@ func (c *cmdClusterBootstrap) Run(cmd *cobra.Command, args []string) error {
 	address := util.NetworkInterfaceAddress()
 	address = util.CanonicalNetworkAddress(address, 7443)
 
-	return m.NewCluster(hostname, address, time.Second*30)
+	return m.NewCluster(hostname, address, time.Minute*5)
 }
--- microceph/cmd/microceph/cluster_join.go
+++ microceph/cmd/microceph/cluster_join.go
@@ -46,5 +46,5 @@ func (c *cmdClusterJoin) Run(cmd *cobra.Command, args []string) error {
 	address := util.NetworkInterfaceAddress()
 	address = util.CanonicalNetworkAddress(address, 7443)
 
-	return m.JoinCluster(hostname, address, args[0], time.Second*30)
+	return m.JoinCluster(hostname, address, args[0], time.Minute*5)
 }
