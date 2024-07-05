2449103cc27f1e7d893cd40527a6ab86a3d0d548
Utkarsh Bhatt <utkarsh.bhatt@canonical.com>
2024-07-05 14:53:44 +0530

    Allow skipping daemon restart post config op
    Signed-off-by: Utkarsh Bhatt <utkarsh.bhatt@canonical.com>

--- microceph/api/types/configs.go
+++ microceph/api/types/configs.go
@@ -3,9 +3,10 @@ package types
 
 // Configs holds the key value pair
 type Config struct {
-	Key   string `json:"key" yaml:"key"`
-	Value string `json:"value" yaml:"value"`
-	Wait  bool   `json:"wait" yaml:"wait"`
+	Key         string `json:"key" yaml:"key"`
+	Value       string `json:"value" yaml:"value"`
+	Wait        bool   `json:"wait" yaml:"wait"`
+	SkipRestart bool   `json:"skip_restart" yaml:"skip_restart"`
 }
 
 // Configs is a slice of configs
