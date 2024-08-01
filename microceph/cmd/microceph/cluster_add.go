1476b21c83afc27648f238cf50f72be0c19040c4
Max Asnaashari <max.asnaashari@canonical.com>
2024-08-01 23:01:51 +0000

    microceph/cmd/microceph: Set 3 hour default timeout for join tokens
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- microceph/cmd/microceph/cluster_add.go
+++ microceph/cmd/microceph/cluster_add.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/canonical/microcluster/v2/microcluster"
 	"github.com/spf13/cobra"
@@ -11,6 +12,8 @@ import (
 type cmdClusterAdd struct {
 	common  *CmdControl
 	cluster *cmdCluster
+
+	flagTokenDuration float64
 }
 
 func (c *cmdClusterAdd) Command() *cobra.Command {
@@ -19,6 +22,8 @@ func (c *cmdClusterAdd) Command() *cobra.Command {
 		Short: "Generates a token for a new server",
 		RunE:  c.Run,
 	}
+
+	cmd.Flags().Float64Var(&c.flagTokenDuration, "timeout", time.Duration(3*time.Hour).Seconds(), "Number of seconds the token will be valid for (Default: 3 hours)")
 
 	return cmd
 }
@@ -33,7 +38,7 @@ func (c *cmdClusterAdd) Run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	token, err := m.NewJoinToken(context.Background(), args[0])
+	token, err := m.NewJoinToken(context.Background(), args[0], time.Duration(c.flagTokenDuration*float64(time.Second)))
 	if err != nil {
 		return err
 	}
