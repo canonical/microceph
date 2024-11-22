fb53a4730c7574fe9a4134da8ae2d97f9411b88d
Peter Sabaini <peter.sabaini@canonical.com>
2024-11-22 19:08:26 +0100

    Use dpkg to install
    Signed-off-by: Peter Sabaini <peter.sabaini@canonical.com>

--- tests/scripts/actionutils.sh
+++ tests/scripts/actionutils.sh
@@ -418,7 +418,7 @@ function install_hurl() {
     VERSION=5.0.1
     downloadurl="https://github.com/Orange-OpenSource/hurl/releases/download/$VERSION/hurl_${VERSION}_amd64.deb"
     curl --location --remote-name $downloadurl
-    sudo apt update && sudo apt install ./hurl_${VERSION}_amd64.deb
+    sudo dpkg -i ./hurl_${VERSION}_amd64.deb
 }
 
 function hurl() {
