canonical/microceph

commit f85ee55b56e32d0965aa25af7f096e756aeb0ab6

Add SSL configuration for RGW (#355)
# Description

Some tools, like [pgBackRest](https://pgbackrest.org/), can currently
only interact with S3-compatible storages if they work with
[SSL/TLS](https://github.com/pgbackrest/pgbackrest/issues/2340). This PR
adds the possibility of enabling RadosGW with SSL/TLS enabled.

The main idea is to use the PostgreSQL charms with MicroCeph so users
can do backups through pgBackRest in bucket without a cloud service
subscription.

## Type of change

- [X] New feature (non-breaking change which adds functionality)

## How Has This Been Tested?

To test, I used the following steps:
1. Generate some SSL files:
```sh
sudo openssl genrsa -out /var/snap/microceph/common/ca.key 2048
sudo openssl req -x509 -new -nodes -key /var/snap/microceph/common/ca.key -days 1024 -out /var/snap/microceph/common/ca.crt -outform PEM
sudo openssl genrsa -out /var/snap/microceph/common/server.key 2048
sudo openssl req -new -key /var/snap/microceph/common/server.key -out /var/snap/microceph/common/server.csr
sudo nano /var/snap/microceph/common/extfile.cnf # and put the following content: subjectAltName = DNS:localhost
sudo openssl x509 -req -in /var/snap/microceph/common/server.csr -CA /var/snap/microceph/common/ca.crt -CAkey /var/snap/microceph/common/ca.key -CAcreateserial -out /var/snap/microceph/common/server.crt -days 365 -extfile /var/snap/microceph/common/extfile.cnf
```
2. Then bootstrap the MicroCeph cluster, enable the RadosGW service with
SSL enabled and create a user:
```sh
sudo microceph cluster bootstrap
sudo microceph disk add loop,4G,3
sudo microceph enable rgw --ssl-certificate=/var/snap/microceph/common/server.crt --ssl-private-key=/var/snap/microceph/common/server.key
sudo microceph.radosgw-admin user create --uid test --display-name test
```
3. To finish, test the access by creating a bucket:
```sh
aws configure # to configure the credentials from RadosGW.

microceph/ceph/rgw.go
@@ -3,25 +3,52 @@ package ceph
 import (
 	"context"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"github.com/canonical/microceph/microceph/constants"
+	"github.com/canonical/microceph/microceph/interfaces"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/canonical/microceph/microceph/constants"
-	"github.com/canonical/microceph/microceph/interfaces"
 
 	"github.com/canonical/microceph/microceph/database"
 )
 
 // EnableRGW enables the RGW service on the cluster and adds initial configuration given a service port number.
-func EnableRGW(s interfaces.StateInterface, port int, monitors []string) error {
+func EnableRGW(s interfaces.StateInterface, port int, sslPort int, sslCertificate string, sslPrivateKey string, monitors []string) error {
 	pathConsts := constants.GetPathConst()
 
+	sslCertificatePath := ""
+	sslPrivateKeyPath := ""
+	if sslCertificate != "" && sslPrivateKey != "" {
+		sslCertificatePath = filepath.Join(pathConsts.SSLFilesPath, "server.crt")
+		decodedSSLCertificate, err := base64.StdEncoding.DecodeString(sslCertificate)
+		if err != nil {
+			return err
+		}
+		err = os.WriteFile(sslCertificatePath, decodedSSLCertificate, 0600)
+		if err != nil {
+			return err
+		}
+		sslPrivateKeyPath = filepath.Join(pathConsts.SSLFilesPath, "server.key")
+		decodedSSLPrivateKey, err := base64.StdEncoding.DecodeString(sslPrivateKey)
+		if err != nil {
+			return err
+		}
+		err = os.WriteFile(sslPrivateKeyPath, decodedSSLPrivateKey, 0600)
+		if err != nil {
+			return err
+		}
+	} else if sslCertificate == "" || sslPrivateKey == "" {
+		port = 80
+	}
 	configs := map[string]any{
-		"runDir":   pathConsts.RunPath,
-		"monitors": strings.Join(monitors, ","),
-		"rgwPort":  port,
+		"runDir":             pathConsts.RunPath,
+		"monitors":           strings.Join(monitors, ","),
+		"rgwPort":            port,
+		"sslPort":            sslPort,
+		"sslCertificatePath": sslCertificatePath,
+		"sslPrivateKeyPath":  sslPrivateKeyPath,
 	}
 
 	// Create RGW configuration.
@@ -71,6 +98,16 @@ func DisableRGW(s interfaces.StateInterface) error {
 	err = os.Remove(filepath.Join(pathConsts.DataPath, "radosgw", "ceph-radosgw.gateway", "keyring"))
 	if err != nil {
 		return fmt.Errorf("failed to remove RGW keyring: %w", err)
+	}
+
+	// Remove the SSL files.
+	err = os.Remove(filepath.Join(pathConsts.SSLFilesPath, "server.crt"))
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("failed to remove RGW SSL Certificate file: %w", err)
+	}
+	err = os.Remove(filepath.Join(pathConsts.SSLFilesPath, "server.key"))
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("failed to remove RGW SSL Private Key file: %w", err)
 	}
 
 	// Remove the configuration.

microceph/ceph/rgw_test.go
@@ -19,6 +19,9 @@ type rgwSuite struct {
 	TestStateInterface *mocks.StateInterface
 }
 
+const validSSLCertificate = `LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURuakNDQW9hZ0F3SUJBZ0lVR0czNU9mWkcrRFdFQytrc2FHalJyTmlXZncwd0RRWUpLb1pJaHZjTkFRRUwKQlFBd1hERUxNQWtHQTFVRUJoTUNWVk14RHpBTkJnTlZCQWdNQmtSbGJtbGhiREVVTUJJR0ExVUVCd3dMVTNCeQphVzVuWm1sbGJHUXhEREFLQmdOVkJBb01BMFJwY3pFWU1CWUdBMVVFQXd3UGQzZDNMbVY0WVcxd2JHVXVZMjl0Ck1CNFhEVEkwTURneE5qRTROREUxT0ZvWERUSTFNRGd4TmpFNE5ERTFPRm93WERFTE1Ba0dBMVVFQmhNQ1ZWTXgKRHpBTkJnTlZCQWdNQmtSbGJtbGhiREVVTUJJR0ExVUVCd3dMVTNCeWFXNW5abWxsYkdReEREQUtCZ05WQkFvTQpBMFJwY3pFWU1CWUdBMVVFQXd3UGQzZDNMbVY0WVcxd2JHVXVZMjl0TUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGCkFBT0NBUThBTUlJQkNnS0NBUUVBdFl5ZGRhb0l4T3hQWmtVMEN1dXE0aEd3Q2JlZXBUM3lBQ0JOS1J6MjB5alQKZ2xSWTFTSTlXSjl4K2t1a3dMTGNiVEIrSkNka2NWTEZuNThtVDRmUW5IMHdmWCtIby9BTUNHNkxITnZnOXovVAorTlV4dTgydGZsVko3RFRUdmVuYzlqVU9qNFZqUExaV2tiemNIOC91Sm1DNkd1ZzAvcksvN2wraG9xNUd6VXhzCmJQeGlOV0QvNW5kaklKa1VidEtpTllnQlRwcnRzZFlCWHoyeTFxS1AxcGZLQ3VIUWVldTNLTWErS0dUU2NUSjYKU251Y0pxZmIvTWdUMWozV3Zpcm1QaUQ3bEwzY3ZmaEtmTEgvYTdsaFhIeDRic21TekZ2UkRYTCt1YmNhak5seQpGUm5WdG9hUHhmMUY4RStFbXh4cXNESlc2bHZKVHJMeW84TjVNbWtoOFFJREFRQUJvMWd3VmpBVUJnTlZIUkVFCkRUQUxnZ2xzYjJOaGJHaHZjM1F3SFFZRFZSME9CQllFRkhIMFoxdWVmSHB1Wll1QTRzRFBlWTd4U2R6b01COEcKQTFVZEl3UVlNQmFBRlBRc1Q0SkU3dUl1ay96T2VvVlZpQVZYeDBoUk1BMEdDU3FHU0liM0RRRUJDd1VBQTRJQgpBUUNucHVFM2hzVHAwckZCU1hWRnV6VzExZjE2bXlML3pyZkJDWnRxQyt6UFZINGlyUUlrRFg2TDdPekY2K00vCml6OFJtQlZXSVpzWTlzczM5SmRlcEsvOVhuMEo5RUdDS2hhdmpldS8yUnpvalFaeXRQWU5DdldtMlhTQ0VHY2wKSDhDcGNQVC9JdnlCNU8yRVl0RUJNcnRrUVNKNjVFWlQyZHRiVFYySUdJN3ZDdjJIUnY0Y2twRXBFTWlLWnNPYgpBcWovbGNLeWFZODVwakFBWWVtVlprZ2dRZTJUM0tzSDFYRVJrNnhFRHF2TUdHbjEvOTNHY1J1enVTVTZaYXVPCmVzVDVISUl2UGZReWlwZG4rOWlKVjluc3hyNGVCa1JPVWFvV2s1NVVENE5tcEtiaHJ3MzZ3RzN4RzJ2RlIxeWUKSVFPNmhKMk5yckFnc2JwemxINzhVcjM4Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0=`
+const validSSLPrivateKey = `LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tCk1JSUV2UUlCQURBTkJna3Foa2lHOXcwQkFRRUZBQVNDQktjd2dnU2pBZ0VBQW9JQkFRQzFqSjExcWdqRTdFOW0KUlRRSzY2cmlFYkFKdDU2bFBmSUFJRTBwSFBiVEtOT0NWRmpWSWoxWW4zSDZTNlRBc3R4dE1INGtKMlJ4VXNXZgpueVpQaDlDY2ZUQjlmNGVqOEF3SWJvc2MyK0QzUDlQNDFURzd6YTErVlVuc05OTzk2ZHoyTlE2UGhXTTh0bGFSCnZOd2Z6KzRtWUxvYTZEVCtzci91WDZHaXJrYk5UR3hzL0dJMVlQL21kMk1nbVJSdTBxSTFpQUZPbXUyeDFnRmYKUGJMV29vL1dsOG9LNGRCNTY3Y294cjRvWk5KeE1ucEtlNXdtcDl2OHlCUFdQZGErS3VZK0lQdVV2ZHk5K0VwOApzZjlydVdGY2ZIaHV5WkxNVzlFTmN2NjV0eHFNMlhJVkdkVzJoby9GL1VYd1Q0U2JIR3F3TWxicVc4bE9zdktqCncza3lhU0h4QWdNQkFBRUNnZ0VBQXVWdTM2RXFTYVh4Y0ZLN1RVOU1KeFljSmxPSkV0N0ZuUTNtM1RpS2tYek4KdnY4RWVjWDFqNVBmbUJ3YjBUMHBPZzZ6ZkhVcWE0cGovN05rdzVFSm1XMS8yQWl3UzhPNUZXdGFDY2hTTXUrUQpQS0IrRGg1dVhaMFR0RkoxYkVxdVRUazBkY0t0ZmhyMGo1ZWhOVnEyVkdObnBLVStyeTkvMDFnd05tMnNVSHNZClBmZWszNjNXRU5BOXlqbDNuOXFicXp4aXphaVowekJEM1ZDWkhXRVBrd215Yk1oRnZQY1V4M24rU2tRUnRhYk0KSzdyZTc2bkwwdU9GdTV3L1FUUU5KVVcvdGJ5R0lZVFJHUExibTFJeWs4RUpmc2lWa09yR0tVWE1HelU1UlZ5QQpROGQvWlI1b3Q0L0R3R29OM2NmQytBODlXT1g2dk5kU3B6Y1VsZmtLS1FLQmdRRGtPbDlIRnRiRklZd0VZbmpDCklQMDdmcVArNnhtVnZpQlRjOEFqQnMraGc1NmhadFltQ29aOTQ4RHJZT0MxSEtWVi9WZWVCU2FHZGo4WUNtdnkKZUNkTExvYms3Skc0bHRiUmxMUlpHUGEyR1JSTkZUZ2xhRXQ2Q1F3QXFuQ1hUMkxoVHI1NGswVlZFT00wclFNWgpUN1NMSVdzZXA5N3N2TW15ZGwyWlNBbDNuUUtCZ1FETHBDSTV4UjF6eU9Kb1RQQStaRTIrYVlCMjdqM0VPeSs2CmdyTXovc3M2c2lndVJ4TEMvRHZJWUNLbXc0S213N2N0dUw3eW5UbDRuaFJIVFhwRDV5M0NLTG5OVTBXRVA5L1MKVmsrS25FUWFFdVdaTm9pbU1xUTNMSkcvL0pYYUpPa2c3WmV0eWt2cnYyUHdLY0lncHpKUGdJWjZOeVRzaDV5NwowbUFyVWF4bFpRS0JnUURON2hXV1VYZE12RzVZYm5uRHdIeCtPRkRGYldEU2lwRWtlNmI4YytMWk82Zmd2cWV2Ci80TkhDRUJFb2s5ZlhBK2JQVkxYbEpJa2RZR01zYXFoUitVOG95aTRXdlZKZDJFeURsbUVvMC9KRTJ3TCtYK0YKMFV0NU83eUd4VU4rWS9VMmt4U3VPMFF0ODJUdlhNVVZDNlErZmRMb0FGVFhpNmo2ekc2OEpoSFV5UUtCZ0VDMApyb3RjcnJjVHBaMHVsVWU5NTFZUmY5aEtheVhuQ0l0aTdENGhQOEl1eWNXcW43T0ZJaG5STWpGNi9oQ3ZMNDAvCm5xekllSEp6Q0U1L3Q5SExxeVorZWt0Ym9rTWJhS3NVOGNGQlZnSlM3dEY0R29OMG8rbEVLQ3V3dm96S0hhbHcKMVRsTGhrUXFWRDhEaGNPS1hOb1dKS1RBME9LM1ZIMzVvc1VnOW41aEFvR0FYYm45dHNOVFp1SmpLWXFxMWszVQovM2trR0NadEJnZmEvaCtpRWdPN1RoZFp5ekdzcjRuVGkzQTFyU09iVkZ0amoza3BOTEZCMW91aTVMcEJjMWFWCkQ0VjhuMHhDdktJbTl2N2hCVm9iTWZVZmVoVE1TSFBZOFZvcWJneXY4ZWZueS9MNVh6d2R3b0NXSGpEZFZXS3EKMVlDLzBIRkhlRFJzWm9aT3RtdTVnTTQ9Ci0tLS0tRU5EIFBSSVZBVEUgS0VZLS0tLS0=`
+
 func TestRGW(t *testing.T) {
 	suite.Run(t, new(rgwSuite))
 }
@@ -67,14 +70,99 @@ func (s *rgwSuite) TestEnableRGW() {
 
 	processExec = r
 
-	err := EnableRGW(s.TestStateInterface, 80, []string{"10.1.1.1", "10.2.2.2"})
+	err := EnableRGW(s.TestStateInterface, 80, 443, "", "", []string{"10.1.1.1", "10.2.2.2"})
 
 	assert.NoError(s.T(), err)
 
 	// check that the radosgw.conf file contains expected values
 	conf := s.ReadCephConfig("radosgw.conf")
-	assert.Contains(s.T(), conf, "rgw frontends = beast port=80")
+	assert.Contains(s.T(), conf, "rgw frontends = beast port=80\n")
 	assert.Contains(s.T(), conf, "mon host = 10.1.1.1,10.2.2.2")
+}
+
+// Test enabling RGW
+func (s *rgwSuite) TestEnableRGWWithInvalidSSLCertificate() {
+	r := mocks.NewRunner(s.T())
+
+	processExec = r
+
+	err := EnableRGW(s.TestStateInterface, 80, 443, "invalid-certificate", validSSLPrivateKey, []string{"10.1.1.1", "10.2.2.2"})
+
+	// we expect an illegal base64 data error
+	assert.EqualError(s.T(), err, "illegal base64 data at input byte 7")
+
+	// check that the radosgw.conf file contains expected values
+	conf := s.ReadCephConfig("radosgw.conf")
+	assert.Equal(s.T(), conf, "")
+}
+
+// Test enabling RGW
+func (s *rgwSuite) TestEnableRGWWithInvalidSSLPrivateKey() {
+	r := mocks.NewRunner(s.T())
+
+	processExec = r
+
+	err := EnableRGW(s.TestStateInterface, 80, 443, validSSLCertificate, "invalid-private-key", []string{"10.1.1.1", "10.2.2.2"})
+
+	// we expect an illegal base64 data error
+	assert.EqualError(s.T(), err, "illegal base64 data at input byte 7")
+
+	// check that the radosgw.conf file contains expected values
+	conf := s.ReadCephConfig("radosgw.conf")
+	assert.Equal(s.T(), conf, "")
+}
+
+// Test enabling RGW
+func (s *rgwSuite) TestEnableRGWWithMissingSSLCertificate() {
+	r := mocks.NewRunner(s.T())
+
+	addRGWEnableExpectations(r)
+
+	processExec = r
+
+	err := EnableRGW(s.TestStateInterface, 0, 443, "", validSSLPrivateKey, []string{"10.1.1.1", "10.2.2.2"})
+
+	assert.NoError(s.T(), err)
+
+	// check that the radosgw.conf file contains expected values
+	conf := s.ReadCephConfig("radosgw.conf")
+	assert.Contains(s.T(), conf, "rgw frontends = beast port=80\n")
+}
+
+// Test enabling RGW
+func (s *rgwSuite) TestEnableRGWWithMissingSSLPrivateKey() {
+	r := mocks.NewRunner(s.T())
+
+	addRGWEnableExpectations(r)
+
+	processExec = r
+
+	err := EnableRGW(s.TestStateInterface, 0, 443, validSSLCertificate, "", []string{"10.1.1.1", "10.2.2.2"})
+
+	assert.NoError(s.T(), err)
+
+	// check that the radosgw.conf file contains expected values
+	conf := s.ReadCephConfig("radosgw.conf")
+	assert.Contains(s.T(), conf, "rgw frontends = beast port=80\n")
+}
+
+// Test enabling RGW
+func (s *rgwSuite) TestEnableRGWWithSSL() {
+	r := mocks.NewRunner(s.T())
+
+	addRGWEnableExpectations(r)
+
+	processExec = r
+
+	err := EnableRGW(s.TestStateInterface, 80, 443, validSSLCertificate, validSSLPrivateKey, []string{"10.1.1.1", "10.2.2.2"})
+
+	assert.NoError(s.T(), err)
+
+	// check that the radosgw.conf file contains expected values
+	conf := s.ReadCephConfig("radosgw.conf")
+	sslCertificatePath := filepath.Join(s.Tmp, "SNAP_COMMON", "server.crt")
+	sslPrivateKeyPath := filepath.Join(s.Tmp, "SNAP_COMMON", "server.key")
+	assert.Contains(s.T(), conf, "rgw frontends = beast port=80 ssl_port=443 ssl_certificate="+sslCertificatePath+" ssl_private_key="+sslPrivateKeyPath+"\n")
 }
 
 func (s *rgwSuite) TestDisableRGW() {

.github/workflows/tests.yml
@@ -167,6 +167,15 @@ jobs:
     - name: Exercise RGW
       run: ~/actionutils.sh testrgw
 
+    - name: Disable RGW
+      run: ~/actionutils.sh disable_rgw
+
+    - name: Enable RGW with SSL enabled
+      run: ~/actionutils.sh enable_rgw_ssl
+
+    - name: Exercise RGW with SSL enabled
+      run: ~/actionutils.sh testrgw_ssl
+
     - name: Test Cluster Config
       run: |
         set -eux

.gitignore
@@ -1,4 +1,5 @@
 *.snap
 _build
 *.swp
+.idea
 .vscode

microceph/ceph/configwriter.go
@@ -87,7 +87,7 @@ auth allow insecure global id reclaim = false
 
 [client.radosgw.gateway]
 rgw init timeout = 1200
-rgw frontends = beast port={{.rgwPort}}
+rgw frontends = beast{{if or (ne .rgwPort 0) (not .sslCertificatePath) (not .sslPrivateKeyPath)}} port={{.rgwPort}}{{end}}{{if and .sslCertificatePath .sslPrivateKeyPath}} ssl_port={{.sslPort}} ssl_certificate={{.sslCertificatePath}} ssl_private_key={{.sslPrivateKeyPath}}{{end}}
 `)),
 		configFile: "radosgw.conf",
 		configDir:  configDir,

microceph/ceph/configwriter_test.go
@@ -45,11 +45,12 @@ func (s *configWriterSuite) TestWriteCephConfig() {
 }
 
 // Test ceph config writing
-func (s *configWriterSuite) TestWriteRadosGWConfig() {
+func (s *configWriterSuite) TestWriteRadosGWNonSSLConfig() {
 	config := newRadosGWConfig(s.Tmp)
 	err := config.WriteConfig(
 		map[string]any{
 			"monitors": "foohost",
+			"rgwPort":  80,
 		},
 		0644,
 	)
@@ -61,6 +62,100 @@ func (s *configWriterSuite) TestWriteRadosGWConfig() {
 	data, err := os.ReadFile(config.GetPath())
 	assert.Equal(s.T(), nil, err)
 	assert.Contains(s.T(), string(data), "foohost")
+	assert.Contains(s.T(), string(data), "rgw frontends = beast port=80\n")
+}
+
+// Test ceph config writing
+func (s *configWriterSuite) TestWriteRadosGWCompleteConfig() {
+	config := newRadosGWConfig(s.Tmp)
+	err := config.WriteConfig(
+		map[string]any{
+			"monitors":           "foohost",
+			"rgwPort":            80,
+			"sslPort":            443,
+			"sslCertificatePath": "/tmp/server.crt",
+			"sslPrivateKeyPath":  "/tmp/server.key",
+		},
+		0644,
+	)
+	assert.Equal(s.T(), nil, err)
+	// Check that the file exists
+	_, err = os.Stat(config.GetPath())
+	assert.Equal(s.T(), nil, err)
+	// Check contents of the file
+	data, err := os.ReadFile(config.GetPath())
+	assert.Equal(s.T(), nil, err)
+	assert.Contains(s.T(), string(data), "foohost")
+	assert.Contains(s.T(), string(data), "rgw frontends = beast port=80 ssl_port=443 ssl_certificate=/tmp/server.crt ssl_private_key=/tmp/server.key")
+}
+
+func (s *configWriterSuite) TestWriteRadosGWSSLOnlyConfig() {
+	config := newRadosGWConfig(s.Tmp)
+	err := config.WriteConfig(
+		map[string]any{
+			"monitors":           "foohost",
+			"rgwPort":            0,
+			"sslPort":            443,
+			"sslCertificatePath": "/tmp/server.crt",
+			"sslPrivateKeyPath":  "/tmp/server.key",
+		},
+		0644,
+	)
+	assert.Equal(s.T(), nil, err)
+	// Check that the file exists
+	_, err = os.Stat(config.GetPath())
+	assert.Equal(s.T(), nil, err)
+	// Check contents of the file
+	data, err := os.ReadFile(config.GetPath())
+	assert.Equal(s.T(), nil, err)
+	assert.Contains(s.T(), string(data), "foohost")
+	assert.Contains(s.T(), string(data), "rgw frontends = beast ssl_port=443 ssl_certificate=/tmp/server.crt ssl_private_key=/tmp/server.key")
+}
+
+func (s *configWriterSuite) TestWriteRadosGWWithMissingSSLCertificateConfig() {
+	config := newRadosGWConfig(s.Tmp)
+	err := config.WriteConfig(
+		map[string]any{
+			"monitors":           "foohost",
+			"rgwPort":            80,
+			"sslPort":            443,
+			"sslCertificatePath": "",
+			"sslPrivateKeyPath":  "/tmp/server.key",
+		},
+		0644,
+	)
+	assert.Equal(s.T(), nil, err)
+	// Check that the file exists
+	_, err = os.Stat(config.GetPath())
+	assert.Equal(s.T(), nil, err)
+	// Check contents of the file
+	data, err := os.ReadFile(config.GetPath())
+	assert.Equal(s.T(), nil, err)
+	assert.Contains(s.T(), string(data), "foohost")
+	assert.Contains(s.T(), string(data), "rgw frontends = beast port=80\n")
+}
+
+func (s *configWriterSuite) TestWriteRadosGWWithMissingSSLPrivateKeyConfig() {
+	config := newRadosGWConfig(s.Tmp)
+	err := config.WriteConfig(
+		map[string]any{
+			"monitors":           "foohost",
+			"rgwPort":            80,
+			"sslPort":            443,
+			"sslCertificatePath": "/tmp/server.crt",
+			"sslPrivateKeyPath":  "",
+		},
+		0644,
+	)
+	assert.Equal(s.T(), nil, err)
+	// Check that the file exists
+	_, err = os.Stat(config.GetPath())
+	assert.Equal(s.T(), nil, err)
+	// Check contents of the file
+	data, err := os.ReadFile(config.GetPath())
+	assert.Equal(s.T(), nil, err)
+	assert.Contains(s.T(), string(data), "foohost")
+	assert.Contains(s.T(), string(data), "rgw frontends = beast port=80\n")
 }
 
 // Test ceph keyring writing

microceph/ceph/services_placement_rgw.go
@@ -8,7 +8,10 @@ import (
 )
 
 type RgwServicePlacement struct {
-	Port int
+	Port           int
+	SSLPort        int
+	SSLCertificate string
+	SSLPrivateKey  string
 }
 
 func (rgw *RgwServicePlacement) PopulateParams(s interfaces.StateInterface, payload string) error {
@@ -32,7 +35,7 @@ func (rgw *RgwServicePlacement) ServiceInit(s interfaces.StateInterface) error {
 		return fmt.Errorf("failed to get config db: %w", err)
 	}
 
-	return EnableRGW(s, rgw.Port, getMonitorAddresses(config))
+	return EnableRGW(s, rgw.Port, rgw.SSLPort, rgw.SSLCertificate, rgw.SSLPrivateKey, getMonitorAddresses(config))
 }
 
 func (rgw *RgwServicePlacement) PostPlacementCheck(s interfaces.StateInterface) error {

microceph/cmd/microceph/enable_rgw.go
@@ -13,19 +13,26 @@ import (
 )
 
 type cmdEnableRGW struct {
-	common     *CmdControl
-	wait       bool
-	flagPort   int
-	flagTarget string
+	common             *CmdControl
+	wait               bool
+	flagPort           int
+	flagSSLPort        int
+	flagSSLCertificate string
+	flagSSLPrivateKey  string
+	flagTarget         string
 }
 
 func (c *cmdEnableRGW) Command() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "rgw [--port <port>] [--target <server>] [--wait <bool>]",
+		Use:   "rgw [--port <port>] [--ssl-port <port>] [--ssl-certificate <certificate material>] [--ssl-private-key <private key material>] [--target <server>] [--wait <bool>]",
 		Short: "Enable the RGW service on the --target server (default: this server)",
 		RunE:  c.Run,
 	}
-	cmd.PersistentFlags().IntVar(&c.flagPort, "port", 80, "Service port (default: 80)")
+	// The flagPort has a default value of 0 for the case where both the SSL certificate and private key are provided.
+	cmd.PersistentFlags().IntVar(&c.flagPort, "port", 0, "Service non-SSL port (default: 80 if no SSL certificate and/or private key are provided)")
+	cmd.PersistentFlags().IntVar(&c.flagSSLPort, "ssl-port", 443, "Service SSL port (default: 443)")
+	cmd.PersistentFlags().StringVar(&c.flagSSLCertificate, "ssl-certificate", "", "base64 encoded SSL certificate")
+	cmd.PersistentFlags().StringVar(&c.flagSSLPrivateKey, "ssl-private-key", "", "base64 encoded SSL private key")
 	cmd.PersistentFlags().StringVar(&c.flagTarget, "target", "", "Server hostname (default: this server)")
 	cmd.Flags().BoolVar(&c.wait, "wait", true, "Wait for rgw service to be up.")
 	return cmd
@@ -43,7 +50,7 @@ func (c *cmdEnableRGW) Run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	jsp, err := json.Marshal(ceph.RgwServicePlacement{Port: c.flagPort})
+	jsp, err := json.Marshal(ceph.RgwServicePlacement{Port: c.flagPort, SSLPort: c.flagSSLPort, SSLCertificate: c.flagSSLCertificate, SSLPrivateKey: c.flagSSLPrivateKey})
 	if err != nil {
 		return err
 	}

commit 05a506804c0afa12f324045b9b4713d414b83ef3

fix: rgw --port is not respected
This regression was introduced in https://github.com/canonical/microceph/pull/355.
This change was tested with an update of the current unit tests.

Closes: #483
Signed-off-by: Neha Oudin <neha.oudin@canonical.com>

microceph/ceph/rgw.go
@@ -40,7 +40,12 @@ func EnableRGW(s interfaces.StateInterface, port int, sslPort int, sslCertificat
 			return err
 		}
 	} else if sslCertificate == "" || sslPrivateKey == "" {
-		port = 80
+		// The default value is in the command line is 0 for the case where
+		// both SSL certificates and Private Key are provided, so we handle the
+		// default case here.
+		if port == 0 {
+			port = 80
+		}
 	}
 	configs := map[string]any{
 		"runDir":             pathConsts.RunPath,

microceph/ceph/rgw_test.go
@@ -65,13 +65,13 @@ func (s *rgwSuite) TestEnableRGW() {
 
 	processExec = r
 
-	err := EnableRGW(s.TestStateInterface, 80, 443, "", "", []string{"10.1.1.1", "10.2.2.2"})
+	err := EnableRGW(s.TestStateInterface, 8081, 443, "", "", []string{"10.1.1.1", "10.2.2.2"})
 
 	assert.NoError(s.T(), err)
 
 	// check that the radosgw.conf file contains expected values
 	conf := s.ReadCephConfig("radosgw.conf")
-	assert.Contains(s.T(), conf, "rgw frontends = beast port=80\n")
+	assert.Contains(s.T(), conf, "rgw frontends = beast port=8081\n")
 	assert.Contains(s.T(), conf, "mon host = 10.1.1.1,10.2.2.2")
 }
 
@@ -149,7 +149,7 @@ func (s *rgwSuite) TestEnableRGWWithSSL() {
 
 	processExec = r
 
-	err := EnableRGW(s.TestStateInterface, 80, 443, validSSLCertificate, validSSLPrivateKey, []string{"10.1.1.1", "10.2.2.2"})
+	err := EnableRGW(s.TestStateInterface, 8081, 443, validSSLCertificate, validSSLPrivateKey, []string{"10.1.1.1", "10.2.2.2"})
 
 	assert.NoError(s.T(), err)
 
@@ -157,7 +157,7 @@ func (s *rgwSuite) TestEnableRGWWithSSL() {
 	conf := s.ReadCephConfig("radosgw.conf")
 	sslCertificatePath := filepath.Join(s.Tmp, "SNAP_COMMON", "server.crt")
 	sslPrivateKeyPath := filepath.Join(s.Tmp, "SNAP_COMMON", "server.key")
-	assert.Contains(s.T(), conf, "rgw frontends = beast port=80 ssl_port=443 ssl_certificate="+sslCertificatePath+" ssl_private_key="+sslPrivateKeyPath+"\n")
+	assert.Contains(s.T(), conf, "rgw frontends = beast port=8081 ssl_port=443 ssl_certificate="+sslCertificatePath+" ssl_private_key="+sslPrivateKeyPath+"\n")
 }
 
 func (s *rgwSuite) TestDisableRGW() {