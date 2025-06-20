8dcb3b134ef8ac8a6ec77c3c530ead69189d9bde
Sharon Koech <sharon.koech@canonical.com>
2025-06-20 15:12:42 +0300

    Add fixes after review
    Signed-off-by: Sharon Koech <sharon.koech@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,7 @@ import os
 import yaml
 
 # Configuration for the Sphinx documentation builder.
-# All configuration specific to your project should be done in this file.
-#
-# If you're new to Sphinx and don't want any advanced or custom features,
-# just go through the items marked 'TODO'.
+# All configuration specific to our project should be done in this file.
 #
 # A complete list of built-in Sphinx configuration values:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -21,8 +18,6 @@ import yaml
 #######################
 
 # Project name
-#
-# TODO: Update with the official name of your project or product
 
 project = "MicroCeph"
 author = "Canonical Ltd."
@@ -30,9 +25,9 @@ author = "Canonical Ltd."
 
 # Sidebar documentation title; best kept reasonably short
 #
-# TODO: To include a version number, add it here (hardcoded or automated).
+# To include a version number, add it here (hardcoded or automated).
 #
-# TODO: To disable the title, set to an empty string.
+# To disable the title, set to an empty string.
 
 html_title = project + " documentation"
 
@@ -42,10 +37,10 @@ html_title = project + " documentation"
 # Now, the starter pack uses CC-BY-SA as the license
 # and the current year as the copyright year.
 #
-# TODO: If your docs need another license, specify it instead of 'CC-BY-SA'.
+# If your docs need another license, specify it instead of 'CC-BY-SA'.
 #
-# TODO: If your documentation is a part of the code repository of your project,
-#       it inherits the code license instead; specify it instead of 'CC-BY-SA'.
+# If your documentation is a part of the code repository of your project,
+# it inherits the code license instead; specify it instead of 'CC-BY-SA'.
 #
 # NOTE: For static works, it is common to provide the first publication year.
 #       Another option is to provide both the first year of publication
@@ -60,36 +55,36 @@ html_title = project + " documentation"
 #         -H 'Accept: application/vnd.github.v3.raw' \
 #         https://api.github.com/repos/canonical/<REPO> | jq '.created_at'
 
-copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
+copyright = "%s AGPL-3.0, %s" % (datetime.date.today().year, author)
 
 
 # Documentation website URL
 #
-# TODO: Update with the official URL of your docs or leave empty if unsure.
+# Update with the official URL of your docs or leave empty if unsure.
 #
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
 
-ogp_site_url = "https://canonical-starter-pack.readthedocs-hosted.com/"
+ogp_site_url = "https://canonical-microceph.readthedocs-hosted.com/"
 
 
 # Preview name of the documentation website
 #
-# TODO: To use a different name for the project in previews, update as needed.
+# To use a different name for the project in previews, update as needed.
 
 ogp_site_name = project
 
 
 # Preview image URL
 #
-# TODO: To customise the preview image, update as needed.
+# To customise the preview image, update as needed.
 
 ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg"
 
 
 # Product favicon; shown in bookmarks, browser tabs, etc.
 
-# TODO: To customise the favicon, uncomment and update as needed.
+# To customise the favicon, uncomment and update as needed.
 
 # html_favicon = '.sphinx/_static/favicon.png'
 
@@ -100,8 +95,8 @@ ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg
 html_context = {
     # Product page URL; can be different from product docs URL
     #
-    # TODO: Change to your product website URL,
-    #       dropping the 'https://' prefix, e.g. 'ubuntu.com/lxd'.
+    # Change to your product website URL,
+    # dropping the 'https://' prefix, e.g. 'ubuntu.com/lxd'.
     #
     # TODO: If there's no such website,
     #       remove the {{ product_page }} link from the page header template
@@ -109,65 +104,65 @@ html_context = {
     "product_page": "",
     # Product tag image; the orange part of your logo, shown in the page header
     #
-    # TODO: To add a tag image, uncomment and update as needed.
+    # To add a tag image, uncomment and update as needed.
     # 'product_tag': '_static/tag.png',
     # Your Discourse instance URL
     #
-    # TODO: Change to your Discourse instance URL or leave empty.
+    # Change to your Discourse instance URL or leave empty.
     #
-    # NOTE: If set, adding ':discourse: 123' to an .rst file
+    # If set, adding ':discourse: 123' to an .rst file
     #       will add a link to Discourse topic 123 at the bottom of the page.
     "discourse": "",
     # Your Mattermost channel URL
     #
-    # TODO: Change to your Mattermost channel URL or leave empty.
+    # Change to your Mattermost channel URL or leave empty.
     "mattermost": "https://chat.canonical.com/canonical/channels/ceph",
     # Your Matrix channel URL
     #
-    # TODO: Change to your Matrix channel URL or leave empty.
+    # Change to your Matrix channel URL or leave empty.
     "matrix": "https://matrix.to/#/#ceph-general:ubuntu.com",
     # Your documentation GitHub repository URL
     #
-    # TODO: Change to your documentation GitHub repository URL or leave empty.
+    # Change to your documentation GitHub repository URL or leave empty.
     #
     # NOTE: If set, links for viewing the documentation source files
     #       and creating GitHub issues are added at the bottom of each page.
     "github_url": "https://github.com/canonical/microceph",
     # Docs branch in the repo; used in links for viewing the source files
     #
-    # TODO: To customise the branch, uncomment and update as needed.
+    # To customise the branch, uncomment and update as needed.
     'repo_default_branch': 'main',
     # Docs location in the repo; used in links for viewing the source files
     #
 
 
-    # TODO: To customise the directory, uncomment and update as needed.
+    # To customise the directory, uncomment and update as needed.
     "repo_folder": "/docs/",
-    # TODO: To enable or disable the Previous / Next buttons at the bottom of pages
+    # To enable or disable the Previous / Next buttons at the bottom of pages
     # Valid options: none, prev, next, both
-    # "sequential_nav": "both",
-    # TODO: To enable listing contributors on individual pages, set to True
+    "sequential_nav": "both",
+    # To enable listing contributors on individual pages, set to True
     "display_contributors": True,
 
     # Required for feedback button    
     'github_issues': 'enabled',
 }
 
-# TODO: To enable the edit button on pages, uncomment and change the link to a
+# To enable the edit button on pages, uncomment and change the link to a
 # public repository on GitHub or Launchpad. Any of the following link domains
 # are accepted:
 # - https://github.com/example-org/example"
 # - https://launchpad.net/example
 # - https://git.launchpad.net/example
 #
-# html_theme_options = {
-# 'source_edit_link': 'https://github.com/canonical/sphinx-docs-starter-pack',
-# }
+html_theme_options = {
+    'source_edit_link': 'https://github.com/canonical/microceph',
+}
 
 # Project slug; see https://meta.discourse.org/t/what-is-category-slug/87897
 #
-# TODO: If your documentation is hosted on https://docs.ubuntu.com/,
-#       uncomment and update as needed.
+# If your documentation is hosted on https://docs.ubuntu.com/,
+# uncomment and update as needed.
 
 # slug = ''
 
@@ -177,7 +172,7 @@ html_context = {
 
 # Base URL of RTD hosted project
 
-html_baseurl = 'https://canonical-microceph.readthedocs-hosted.com/en/squid-stable/'
+html_baseurl = 'https://canonical-microceph.readthedocs-hosted.com/'
 
 # URL scheme. Add language and version scheme elements.
 # When configured with RTD variables, check for RTD environment so manual runs succeed:
@@ -216,7 +211,7 @@ redirects = {}
 
 # A regex list of URLs that are ignored by 'make linkcheck'
 #
-# TODO: Remove or adjust the ACME entry after you update the contributing guide
+# Remove or adjust the ACME entry after you update the contributing guide
 
 linkcheck_ignore = [
     "http://127.0.0.1:8000",
@@ -227,7 +222,11 @@ linkcheck_ignore = [
 
 # A regex list of URLs where anchors are ignored by 'make linkcheck'
 
-linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
+linkcheck_anchors_ignore_for_url = [
+    r"https://github\.com/.*",
+    r"https://app.element.io/#/room/#ceph-general:ubuntu.com",
+    r"https://matrix.to/#/#documentation:ubuntu.com"
+    ]
 
 # give linkcheck multiple tries on failure
 # linkcheck_timeout = 30
@@ -297,14 +296,14 @@ rst_epilog = """
 
 # Feedback button at the top; enabled by default
 #
-# TODO: To disable the button, uncomment this.
+# To disable the button, uncomment this.
 
 # disable_feedback_button = True
 
 
 # Your manpage URL
 #
-# TODO: To enable manpage links, uncomment and replace {codename} with required
+# To enable manpage links, uncomment and replace {codename} with required
 #       release, preferably an LTS release (e.g. noble). Do *not* substitute
 #       {section} or {page}; these will be replaced by sphinx at build time
 #
@@ -343,6 +342,6 @@ if os.path.exists('./reuse/substitutions.yaml'):
 
 # Add configuration for intersphinx mapping
 
-intersphinx_mapping = {
-    'starter-pack': ('https://canonical-example-product-documentation.readthedocs-hosted.com/en/latest', None)
-}
+# intersphinx_mapping = {
+#    'starter-pack': ('https://canonical-example-product-documentation.readthedocs-hosted.com/en/latest', None)
+#}
